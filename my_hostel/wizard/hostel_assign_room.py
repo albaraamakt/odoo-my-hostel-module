864236b1b276a6a4560824b7a4ae57216c0e1f19
Albaraa Maktabi <albaraamakt@gmail.com>
2024-10-08 12:47:13 +0300

    [ADD] hostel_assign_room_wizard

--- my_hostel/wizard/hostel_assign_room.py
+++ my_hostel/wizard/hostel_assign_room.py
@@ -1,8 +1,12 @@
 
+import logging
 from odoo import fields, models
+
+_logger = logging.getLogger(__name__)
 
 class HostelAssignRoom(models.TransientModel):
     _name = "hostel.assign.room.wizard"
+    _description = "Hostel Assign Room Wizard"
 
     # active = fields.Boolean("Active", default=True)
     room_id = fields.Many2one("hostel.room", string="Room", required=True)
@@ -16,3 +20,10 @@ class HostelAssignRoom(models.TransientModel):
                 'room_id': self.room_id.id,
                 'admission_date': fields.Date.today(),
             })
+        rooms = self.mapped('room_id')
+        action = rooms.get_formview_action()
+        if len(rooms.ids) > 1:
+            _logger.debug(msg='if block in add_room_in_student is reached.')
+            action['domain'] = [('id', 'in', tuple(rooms.ids))]
+            action['view_mode'] = 'tree,form'
+        return action
